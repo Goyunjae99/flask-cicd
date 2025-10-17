afd495c7335341bcf2ce4139bfff3808a88f80c7
Goyunjae99 <myben0000@gmail.com>
2025-10-17 17:49:24 +0900

    Change greeting message in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Jenkins CI/CD!"
+    return "LG통합우승 절대 안돼"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
